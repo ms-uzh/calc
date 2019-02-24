53345925ff1ec1a7242d9de5d8271d2f9f1d5348
Florian Forster <ffo@rootd.ch>
2019-02-24 17:19:16 +0100

    a little improvement

--- main.go
+++ main.go
@@ -2,36 +2,64 @@ package main
 
 import "fmt"
 
+// TODO Comment
 const decay = 1.00782503223
 
-var head = 111.11111
-var pol1 = 222.22222
-var tail = 333.33333
-
-func main() {
-	allInputs := []float64{head, pol1, tail}
-	total := calcTotal(allInputs)
-	fmt.Println("the total is: ", total)
-	headtotal := calcHead(total, tail)
-	fmt.Println("the head is: ", headtotal)
-	tailtotal := calcTail(total, head)
-	fmt.Println("the head is: ", tailtotal)
+type head struct {
+	mass float64
 }
 
-func calcTotal(allInputs []float64) float64 {
+type polyamineUnit struct {
+	mass            float64
+	quaternaryamine bool
+}
+
+type tail struct {
+	mass            float64
+	quaternaryamine bool
+}
+
+type result struct {
+	fragments []float64
+}
+
+func main() {
+	// Set some Testparams
+	head := head{
+		mass: 111.11111,
+	}
+	poly := polyamineUnit{
+		mass: 222.22222,
+	}
+	tail := tail{
+		mass: 555.55555,
+	}
+
+	total := calcTotal(head.mass, poly.mass, tail.mass)
+	fmt.Println("hop: ", total)
+
+	headtotal := calcHead(tail.mass, total)
+	fmt.Println("the head is: ", headtotal)
+
+	tailtotal := calcHead(head.mass, total)
+	fmt.Println("the head is: ", tailtotal)
+
+}
+
+func calcTotal(input ...float64) float64 {
 	total := 0.0
-	for _, value := range allInputs {
+	for _, value := range input {
 		total += value
 	}
 	return total
 }
 
-func calcHead(total, tail float64) float64 {
+func calcHead(tail, total float64) float64 {
 	x := total - tail
 	return x
 }
 
-func calcTail(total, head float64) float64 {
+func calcTail(head, total float64) float64 {
 	y := total - head
 	return y
 }
