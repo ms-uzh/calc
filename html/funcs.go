d9c5a7a3028db568b41542ad76533692c5d68c3d
adlerhurst <silvan.reusser@gmail.com>
2019-08-05 12:37:39 +0200

    redesign

--- html/funcs.go
+++ html/funcs.go
@@ -1,6 +1,7 @@
 package html
 
 import (
+	"encoding/base64"
 	"fmt"
 
 	"github.com/fforootd/calc/templates"
@@ -12,9 +13,15 @@ func (s *Server) getFuncs() map[string]interface{} {
 		"getCalcAddress":       s.generateGetCalcAddress(),
 		"generateChemicalName": templates.GenerateChemicalName,
 		"getCSSPath":           s.generateCSSPath(),
+		"getJavascriptPath":    s.generateJavascriptPath(),
 		"round":                templates.Round,
 		"index":                templates.Index,
+		"toBase64":             toBase64,
 	}
+}
+
+func toBase64(text string) string {
+	return base64.StdEncoding.EncodeToString([]byte(text))
 }
 
 func (s *Server) generateGetCalcAddress() func() string {
@@ -29,5 +36,11 @@ func (s *Server) generateGetCalcAddress() func() string {
 func (s *Server) generateCSSPath() func() string {
 	return func() string {
 		return "/resources/css/styles.css"
+	}
+}
+
+func (s *Server) generateJavascriptPath() func() string {
+	return func() string {
+		return "/resources/js"
 	}
 }
